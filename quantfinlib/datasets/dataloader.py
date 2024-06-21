3ee8dd4e483910a2094d0e123fd52b04cdb21309
fluentnumbers <fluentnumbers@gmail.com>
2024-06-21 20:23:05 +0200

    add dataloader tests

--- quantfinlib/datasets/dataloader.py
+++ quantfinlib/datasets/dataloader.py
@@ -1,8 +1,18 @@
+import logging
 from pathlib import Path
 
 import pandas as pd
 
 from quantfinlib.util.fs_utils import get_project_root
+
+logger = logging.getLogger("main")
+
+VIX_ONLINE_URL = (
+    "https://cdn.cboe.com/api/global/us_indices/daily_prices/VIX_History.csv"
+)
+VIX_LOCAL_PATH = (
+    get_project_root() / "quantfinlib" / "datasets" / "data" / "VIX_History.pkl"
+)
 
 
 def load_VIX(load_latest: bool = False) -> pd.DataFrame:
@@ -23,31 +33,28 @@ def load_VIX(load_latest: bool = False) -> pd.DataFrame:
     --------
     >>> df = load_vix()
     """
+    LOAD_LATEST_SUCCESS = False
     if load_latest:
         try:
-            df = pd.read_csv(
-                "https://cdn.cboe.com/api/global/us_indices/daily_prices/VIX_History.csv"
-            )
-            LOAD_SUCCESS = True
+            df = pd.read_csv(VIX_ONLINE_URL)
+            LOAD_LATEST_SUCCESS = True
         except Exception as e:
-            LOAD_SUCCESS = False
             raise RuntimeError(
                 f"Failed to load the latest VIX data: {e}. Proceed with the local data."
             )
 
-    if not load_latest or not LOAD_SUCCESS:
-        data_filename = "VIX_History.pkl"
-        data_path = (
-            get_project_root() / "quantfinlib" / "datasets" / "data" / data_filename
-        )
-        if not data_path.exists():
+    if not load_latest or not LOAD_LATEST_SUCCESS:
+        if not VIX_LOCAL_PATH.exists():
             raise FileNotFoundError(
-                f"VIX dataset file '{data_filename}' does not exist at '{data_path}'"
+                f"VIX dataset file '{VIX_LOCAL_PATH.name}' does not exist at '{VIX_LOCAL_PATH.parent}'"
             )
 
-        df = pd.read_pickle(data_path, compression="gzip")
+        df = pd.read_pickle(VIX_LOCAL_PATH, compression="gzip")
 
     df["DATE"] = pd.to_datetime(df["DATE"])
+    logger.info(
+        f"Loaded VIX dataset with {df.shape[0]} rows and {df.shape[1]} columns. Latest date: {df['DATE'].max()}"
+    )
     return df
 
 
---
+++ tests/datasets/test_dataloader.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+
+import pandas as pd
+import pytest
+
+from quantfinlib.datasets.dataloader import load_VIX
+
+
+@pytest.fixture
+def vix_data():  # Fixture to provide a sample DataFrame for testing
+    data = {
+        "DATE": pd.date_range("2022-01-01", periods=10),
+        "Open": [100, 101, 102, 103, 104, 105, 106, 107, 108, 109],
+        "Close": [99, 100, 101, 102, 103, 104, 105, 106, 107, 108],
+        "High": [105, 106, 107, 108, 109, 110, 111, 112, 113, 114],
+        "Low": [98, 99, 100, 101, 102, 103, 104, 105, 106, 107],
+    }
+    return pd.DataFrame(data)
+
+
+def test_load_VIX_local(monkeypatch):
+    monkeypatch.setattr(
+        "quantfinlib.util.fs_utils.get_project_root", lambda: Path("/non/existent/path")
+    )
+    # monkeypatch.setattr(pd, "read_pickle", lambda path, **kwargs: vix_data)
+    df = load_VIX()
+
+    assert isinstance(df, pd.DataFrame)
+
+    required_columns = ["DATE", "HIGH", "LOW", "CLOSE", "OPEN"]
+    assert all(column in df.columns for column in required_columns)
+
+    assert set(df.columns) == set(required_columns)
+
+
+# write test_load_VIX_online
+def test_load_VIX_online(monkeypatch):
+
+    try:
+        df = load_VIX(load_latest=True)
+    except RuntimeError:
+        pytest.fail("load_VIX() raised RuntimeError unexpectedly!")
+
+    assert isinstance(df, pd.DataFrame)
+
+    required_columns = ["DATE", "HIGH", "LOW", "CLOSE", "OPEN"]
+    assert all(column in df.columns for column in required_columns)
+
+    assert set(df.columns) == set(required_columns)
