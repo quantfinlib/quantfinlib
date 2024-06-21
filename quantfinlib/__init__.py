42562fdb19186c41baea1e30504ead2e0b884063
fluentnumbers <fluentnumbers@gmail.com>
2024-06-21 18:28:24 +0200

    logging config

--- quantfinlib/__init__.py
+++ quantfinlib/__init__.py
@@ -1,1 +1,11 @@
+import logging
+import logging.config
+import yaml
+
 __version__ = "0.0.4"
+
+# Configure logging
+with open(get_project_root("config/logger/base.yaml"), "r") as stream:
+    config = yaml.safe_load(stream)
+logging.config.dictConfig(config)
+logger = logging.getLogger("quantfinlib_logger")
