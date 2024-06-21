580b839ef05adcec6f31d20ce72b10d5bb91a369
Andrejs Fedjajevs <fluentnumbers@gmail.com>
2024-06-21 19:12:23 +0200

    Feature logging (#4)
    * logging config
    
    * rename logger to 'main'
    
    * add fs_utils.py
    
    * remove redundant comments
    
    * added pyyaml dev dependency

--- quantfinlib/__init__.py
+++ quantfinlib/__init__.py
@@ -1,1 +1,15 @@
+import logging
+import logging.config
+
+import yaml
+
+from quantfinlib.util.fs_utils import get_project_root
+
 __version__ = "0.0.4"
+
+# Configure logging
+with open(get_project_root("config/logger/base.yaml"), "r") as stream:
+    config = yaml.safe_load(stream)
+logging.config.dictConfig(config)
+logger = logging.getLogger("main")
+logger.info("Logger configured successfully")
