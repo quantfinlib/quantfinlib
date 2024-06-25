7c0568cdc54c23133d668a424b4c011ced718286
fluentnumbers <fluentnumbers@gmail.com>
2024-06-25 11:14:51 +0200

    move logger outside __init__ into utils

--- quantfinlib/util/fs_utils.py
+++ quantfinlib/util/fs_utils.py
@@ -1,5 +1,9 @@
+import logging
+import logging.config
 from pathlib import Path
 from typing import Union
+
+import yaml
 
 
 def get_project_root(relative_filepath: Union[Path, str, None] = None) -> Path:
@@ -16,6 +20,22 @@ def get_project_root(relative_filepath: Union[Path, str, None] = None) -> Path:
     if relative_filepath is not None:
         root_path = root_path / relative_filepath
     return root_path
+
+
+def get_logger() -> logging.Logger:
+    """Configures the logger using the base.yaml configuration file.
+
+    Returns
+    -------
+    logging.Logger
+        The logger object with the configuration settings.
+    """
+    with open(get_project_root("config/logger/base.yaml"), "r") as stream:
+        config = yaml.safe_load(stream)
+    logging.config.dictConfig(config)
+    logger = logging.getLogger("main")
+    logger.info("Logger configured successfully")
+    return logger
 
 
 if __name__ == "__main__":
