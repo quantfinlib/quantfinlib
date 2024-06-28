53c97fc7d013c1456d2f4faea55bc40c3b808823
Thijs van den Berg <thijs@sitmo.com>
2024-06-28 11:18:37 +0200

    refactoring, moving convenience close to base class

---
+++ quantfinlib/sim/_base.py
@@ -0,0 +1,191 @@
+
+from typing import Union, Optional
+import pandas as pd
+import numpy as np
+from quantfinlib._datatypes.timeseries import time_series_freq_to_duration
+
+
+class SimHelperBase:
+    def __init__(self):
+        # Info about the data used for fitting        
+        self.fit_container_dtype_ = None
+
+        self.fit_num_rows_ = None
+        self.fit_num_cols_ = None
+
+        self.fit_index_name_ = None
+        self.fit_index_min_ = None
+        self.fit_index_max_ = None
+        self.fit_index_freq_ = None
+
+        self.fit_column_names_ = None
+        
+        # Additional info from the fit() call we might need later
+        self.fit_dt_ = None
+        self.fit_x0_ = None
+        self.fit_xn_ = None
+
+    def inspect_and_normalize_fit_args(
+        self,
+        x: Union[np.ndarray, pd.DataFrame, pd.Series],
+        dt: Optional[float]):
+
+        self.fit_container_dtype_ = type(x)
+
+        # Collect and remember DatetimeIndex data if available
+        if isinstance(x, (pd.DataFrame, pd.Series)):
+            values = x.to_numpy()
+            if isinstance(x, pd.DataFrame):
+                self.fit_column_names_ = x.columns.values.tolist()
+            elif isinstance(x, pd.Series):
+                self.fit_column_names_ = [x.name]
+            if isinstance(x.index, pd.DatetimeIndex):
+                self.fit_index_name_ = x.index.name
+                self.fit_index_min_ = x.index.min()
+                self.fit_index_max_ = x.index.max()
+                self.fit_index_freq_ = pd.infer_freq(x.index)
+        else: # np.ndarray
+            values = x
+
+        if values.ndim == 1:
+            values = values.reshape(-1, 1)
+        
+        # if dt is not provided then we need to infer it
+        if dt is None:
+            if self.fit_index_freq_ is not None:
+                dt = time_series_freq_to_duration(self.fit_index_freq_)
+            else:
+                raise ValueError("Unable to determine dt")
+
+        # validation
+        assert values.ndim == 2
+        assert values.shape[0] >= 3
+        assert dt > 0.0
+
+        # Save fitting information
+        self.fit_dt_ = dt
+        self.fit_num_rows_ = values.shape[0]
+        self.fit_num_cols_ = values.shape[1]
+        self.fit_x0_ = values[0, :].reshape(1, -1)
+        self.fit_xn_ = values[-1, :].reshape(1, -1)
+
+        return values, dt
+
+    def normalize_sim_path_args(
+        self,
+        x0: Optional[Union[float, np.ndarray, pd.DataFrame, pd.Series, str]] = None,
+        dt: Optional[float] = None,
+        label_start = None,
+        label_freq: Optional[str] = None):
+
+        need_labels = (
+            (self.fit_index_min_ is not None) 
+            or (label_start is not None)
+            or (label_freq is not None)
+        )
+
+        is_fitted = (self.fit_x0_ is not None)
+
+        # Defaults for x0
+        if x0 is None:
+            if is_fitted:
+                x0 = self.fit_x0_
+                if need_labels and (label_start is None):
+                    label_start = self.fit_index_min_
+            else:
+                x0 = np.array([[0.0]])
+        elif x0 == "first":
+            if not is_fitted:
+                raise ValueError('x0: "first" can not be used because the model is not fitted.')
+            x0 = self.fit_x0_
+            if need_labels and (label_start is None):
+                label_start = self.fit_index_min_
+        elif x0 == "last":
+            if not is_fitted:
+                raise ValueError('x0: "last" can not be used because the model is not fitted.')            
+            x0 = self.fit_xn_
+            if need_labels and (label_start is None):
+                label_start = self.fit_index_max_
+        elif isinstance(x0, str):
+            raise ValueError(f'x0: Unknown string value "{x0}", valid string values are "first" or "last".')
+        elif isinstance(x0, float):
+            x0 = np.ndarray([[x0]])
+        elif isinstance(xp0, list):
+            x0 = np.ndarray(x0)
+        elif isinstance(x0, (pd.DataFrame, pd.Series)):
+            x0 = x0.to_numpy()
+        assert isinstance(x0, np.ndarray)
+
+        x0 = x0.reshape(1, -1)
+
+        # if dt not t is provided then align dt with the fitting()
+        if dt is None:
+            if is_fitted:
+                dt = self.fit_dt_
+            else:
+                dt = 1.0/252.0
+        assert (dt is not None)
+        assert (dt > 0)
+
+        # is freq is missing, use the freq we saw while fitting
+        if need_labels and (label_freq is None):
+            label_freq = self.fit_index_freq_
+
+        return x0, dt, label_start, label_freq
+    
+    def format_ans(
+        self,
+        ans: np.ndarray,
+        label_start: Optional[str],
+        label_end: Optional[str]
+        ):
+
+        need_labels = (
+            (self.fit_index_min_ is not None) 
+            or (label_start is not None)
+            or (label_freq is not None)
+        )
+
+        is_fitted = (self.fit_x0_ is not None)
+
+        
+        # configure the return container
+        if need_labels:
+            assert label_start is not None
+            assert label_freq is not None
+            index = pd.date_range(
+                start=label_start, 
+                freq=label_freq, 
+                periods=num_num_steps + 1,
+                name=self.fit_index_name_
+            )
+
+            # Return a series if we fitted with a series, and if we only have 1 scenario
+            if isinstance(self.fit_container_dtype_, pd.Series) and (ans.shape[1] == 1):
+                ans = pd.Series(
+                    data = ans,
+                    index = index,
+                    name = self.fit_column_names_[0] + '_0'
+                )
+            else:
+                columns = [f"{c}_{i}" for i in range(num_paths) for c in self.fit_column_names_]
+
+                ans = pd.DataFrame(
+                    data = ans,
+                    index= index,
+                    columns = columns
+                )
+
+        # Return ans, potentially strip away the first
+        if include_x0:
+            return ans
+        else:
+            if isinstance(ans, (pd.DataFrame, pd.Series)):
+                return ans.iloc[1:, :]
+            else:
+                return ans[1:, :]
+
+    @staticmethod
+    def set_x0(ans: np.ndarray, x0: np.ndarray):
+        x0 = x0.reshape(1, -1)
+        ans[0, :] = np.tile(x0, ans.shape[1] // x0.shape[1])
--- quantfinlib/sim/_bm.py
+++ quantfinlib/sim/_bm.py
@@ -8,14 +8,15 @@ BrownianMotion()
 
 __all__ = ["BrownianMotion"]
 
+
 from typing import Union, Optional
 from sklearn.base import BaseEstimator
 import pandas as pd
 import numpy as np
-from quantfinlib._datatypes.timeseries import time_series_freq_to_duration
+from quantfinlib.sim._base import SimHelperBase
 
 
-class BrownianMotionBase(BaseEstimator):
+class BrownianMotionBase(BaseEstimator, SimHelperBase):
     """Brownian Motion base class"""
 
     def __init__(self, drift=0.0, vol=0.1, cor=None):
@@ -23,38 +24,19 @@ class BrownianMotionBase(BaseEstimator):
         # Parameters
         self.drift = np.asarray(drift).reshape(1, -1)
         self.vol = np.asarray(vol).reshape(1, -1)
-        self.cor = None
 
         # Private attributes
-        if self.cor is None:
+        if cor is None:
+            self.cor = None
             self.L_ = None
         else:
+            self.cor = np.asarray(cor)
             self.L_ = np.linalg.cholesky(self.cor)
 
-        # Info about the data used for fitting        
-        self.fit_dt_ = None
-        self.fit_num_rows_ = None
-        self.fit_num_cols_ = None
-        self.fit_x0_ = None
-        self.fit_xn_ = None
         
     def fit(self, x: np.ndarray, dt: float):
         
-        # Preprocessing arguments
-        if x.ndim == 1:
-            x = x.reshape(-1, 1)
-        
-        # validation
-        assert x.ndim == 2
-        assert x.shape[0] >= 3
-        assert dt > 0.0
-        
-        # Save fitting information
-        self.fit_dt_ = dt
-        self.fit_num_rows_ = x.shape[0]
-        self.fit_num_cols_ = x.shape[1]
-        self.fit_x0_ = x[0, :].reshape(1, -1)
-        self.fit_xn_ = x[-1, :].reshape(1, -1)
+        x = SimHelperBase.inspect_and_normalize_fit_args(x, dt)
         
         # changes from one row to the next
         dx = np.diff(x, axis=0)
@@ -81,26 +63,13 @@ class BrownianMotionBase(BaseEstimator):
         num_paths: int,
         random_state: Optional[int] = None
     ) -> np.ndarray:
-            
-        # validation
-        assert dt > 0
-        assert x0.ndim == 2 # a matrix
-        assert x0.shape[0] == 1 # a row
-        assert x0.shape[1] == self.drift.shape[1]
-
+    
         # Allocate storage for the simulation
         num_cols = self.drift.shape[1]
         ans = np.zeros(shape=(num_steps + 1, num_cols * num_paths))
 
-        # set the initial value
-        if x0.shape[1] == ans.shape[1]:
-            ans[0, :] = x0
-        elif x0.shape[1] == self.drift.shape[1]:
-            ans[0, :] = np.repeat(x0, num_paths, axis=1)
-        elif x0.shape[1] == 1:
-            ans[0, :] = x0
-        else:
-            raise ValueError(f'The shape of x0 {x0.shape} is incompatible with the target output {ans.shape}')
+        # set the initial value of the simulation
+        SimHelperBase.set_x0(ans, x0)
         
         # Create a Generator instance with the seed
         rng = np.random.default_rng(random_state)
@@ -127,18 +96,11 @@ class BrownianMotionBase(BaseEstimator):
         return ans
 
 
-
 class BrownianMotion(BrownianMotionBase):
     """Brownian Motion simulation"""
 
     def __init__(self, drift=0.0, vol=0.1, cor=None):
         super().__init__(drift, vol, cor)
-
-        self.fit_container_dtype_ = None
-        self.fit_index_min_ = None
-        self.fit_index_max_ = None
-        self.fit_index_freq_ = None
-        self.fit_dt_ = None
 
     def fit(
         self,
@@ -146,30 +108,8 @@ class BrownianMotion(BrownianMotionBase):
         dt: Optional[float],
         **kwargs
     ):
-        # 
-        self.fit_container_dtype_ = type(x)
-        
-        # Collect and remember DatetimeIndex data if available
-        # also make "value" a numpy array
-        if isinstance(x, (pd.DataFrame, pd.Series)):
-            values = x.to_numpy()
-            if isinstance(x.index, pd.DatetimeIndex):
-                self.fit_index_min_ = x.index.min()
-                self.fit_index_max_ = x.index.max()
-                self.fit_index_freq_ = pd.infer_freq(x.index)
-        else: # np.ndarray
-            values = x
-        
-        # if dt is not provided then we need to infer it
-        if dt is None:
-            if self.index_freq_ is not None:
-                dt = time_series_freq_to_duration(self.index_freq_)
-            else:
-                raise ValueError("Unable to determine dt")
-        self.dt_ = dt
-       
-        # do the actual fitting in the base class
-        super().fit(values, dt)
+        values, dt = SimHelperBase.inspect_and_normalize_fit_args(x, dt)
+        BrownianMotionBase.fit(values, dt)
         return self
 
     def sim_path(
@@ -184,97 +124,13 @@ class BrownianMotion(BrownianMotionBase):
         include_x0: bool = True,
     ) -> Union[np.ndarray, pd.DataFrame, pd.Series]:
 
-        # Are we going to attempt to return a pandas object with DataTimeIndex?
-        # we will try this if 
-        # 1) we had a DateTimeIndex in the fit(x) dataset
-        # 2) if one of of label_start or label_freq was set in this call
-        return_datetime_index = (
-            (self.fit_index_min_ is not None) 
-            or (label_start is not None)
-            or (label_freq is not None)
-        )
-
-        is_fitted = (self.fit_x0_ is not None)
-
-        # Defaults for x0
-        if x0 is None:
-            if is_fitted:
-                x0 = self.fit_x0_
-                if return_datetime_index and (label_start is None):
-                    label_start = self.fit_index_min_
-            else:
-                x0 = np.array([[0.0]])
-        elif x0 == "first":
-            if not is_fitted:
-                raise ValueError('x0: "first" can not be used because the model is not fitted.')
-            x0 = self.fit_x0_
-            if return_datetime_index and (label_start is None):
-                label_start = self.fit_index_min_
-        elif x0 == "last":
-            if not is_fitted:
-                raise ValueError('x0: "last" can not be used because the model is not fitted.')            
-            x0 = self.fit_xn_
-            if return_datetime_index and (label_start is None):
-                label_start = self.fit_index_max_
-        elif isinstance(x0, str):
-            raise ValueError(f'x0: Unknown string value "{x0}", valid string values are "first" or "last".')
-        elif isinstance(x0, float):
-            x0 = np.ndarray([[x0]])
-        elif isinstance(xp0, list):
-            x0 = np.ndarray(x0)
-        elif isinstance(x0, (pd.DataFrame, pd.Series)):
-            x0 = x0.to_numpy()
-        assert isinstance(x0, np.ndarray)
-
-        x0 = x0.reshape(1, -1)
-
-        # if dt not t is provided then align dt with the fitting()
-        if dt is None:
-            if is_fitted:
-                dt = self.fit_dt_
-            else:
-                dt = 1.0/252.0
-        assert (dt is not None)
-        assert (dt > 0)
-
-        # is freq is missing, use the freq we saw while fitting
-        if return_datetime_index and (label_freq is None):
-            label_freq = self.fit_index_freq_
-        
+        # handle arg defaults
+        x0, dt, label_start, label_freq = SimHelperBase.normalize_sim_path_args(x0, dt, label_start, label_freq)
+                
         # do the sims
-        ans = super().sim_path(x0, dt, num_steps, num_paths, random_state)
+        ans = BrownianMotionBase.sim_path(x0, dt, num_steps, num_paths, random_state)
 
-        # configure the return container
-        if return_datetime_index:
-            assert label_start is not None
-            assert label_freq is not None
-            index = pd.date_range(start=label_start, freq=label_freq, periods=num_num_steps + 1)
-
-            # should we return a series?
-            if isinstance(self.fit_container_dtype_, pd.Series) and (ans.shape[1] == 1):
-                ans = pd.Series(
-                    data = ans,
-                    index = index,
-                    name = self.fit_column_names_[0] + '_0'
-                )
-            else:
-                columns = []
-                for i in range(num_paths):
-                    for c in self.fit_column_names_:
-                        columns.append(f'{c}_{i}')
-                ans = pd.DataFrame(
-                    data = ans,
-                    index= index,
-                    columns = columns
-                )
-
-        # Return ans, potentially strip away the first
-        if include_x0:
-            return ans
-        else:
-            if isinstance(ans, (pd.DataFrame, pd.Series)):
-                return ans.iloc[1:, :]
-            else:
-                return ans[1:, :]
+        # format the ans
+        ans = SimHelperBase.format_ans(ans, label_start, label_freq, include_x0)
 
         return ans
