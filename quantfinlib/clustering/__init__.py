858c637552dd963abf7fbd761f3e7d8901c5c1d2
mjvakili <mohammadjavad.vakili@shell.com>
2024-07-17 16:55:30 +0200

    fixed init in clustering

--- quantfinlib/clustering/__init__.py
+++ quantfinlib/clustering/__init__.py
@@ -1,4 +1,4 @@
 """Module for Clustering analysis."""
 
 from quantfinlib.clustering.hierarchical import HC, optimize_n_clusters
-from quantfinlib.metrics import gap_statistic, silhouette_tstat
+from quantfinlib.clustering.metrics import gap_statistic, silhouette_tstat
