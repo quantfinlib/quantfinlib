be8c6bfd5687646494b79f37d50869d4caa9817e
mjvakili <mohammadjavad.vakili@gmail.com>
2024-09-22 22:18:32 +0200

    added roll_spread to microstructure features

--- quantfinlib/feature/micro_structure/price_sequences.py
+++ quantfinlib/feature/micro_structure/price_sequences.py
@@ -4,46 +4,48 @@ from functools import partial
 
 import numpy as np
 
-from quantfinlib.feature.transform.moving_window_func import moving_average, _moving_generic_1
+from quantfinlib.feature.transform.moving_window_func import moving_average, _moving_generic_1, moving_cov
 
 
-def high_low_vol(high: np.ndarray, low: np.ndarray, window: int) -> np.ndarray:
+def high_low_vol(p_high: np.ndarray, p_low: np.ndarray, window: int) -> np.ndarray:
     """
-    Calculate the high-low volatility.
-
-    See page 284 of Advances in Financial Machine Learning by Marcos Lopez de Prado.
+    Calculate the p_high-p_low volatility.
 
     Parameters
     ----------
-    high : np.ndarray
-        The high prices.
-    low : np.ndarray
-        The low prices.
+    p_high : np.ndarray
+        The p_high prices.
+    p_low : np.ndarray
+        The p_low prices.
     window : int
         The window size.
 
     Returns
     -------
     np.ndarray
-        The high-low volatility.
+        The p_high-p_low volatility.
+
+    References
+    ----------
+    See page 285 of Advances in Financial Machine Learning by Marcos Lopez de Prado.
     """
 
-    sigma_sq = moving_average(x=(np.log(high / low)) ** 2.0, window_size=window) / (4.0 * np.log(2.0))
+    sigma_sq = moving_average(x=(np.log(p_high / p_low)) ** 2.0, window_size=window) / (4.0 * np.log(2.0))
     return sigma_sq
 
 
-def cowrin_schultz_spread(high: np.ndarray, low: np.ndarray, window: int) -> np.ndarray:
+def cowrin_schultz_spread(p_high: np.ndarray, p_low: np.ndarray, window: int) -> np.ndarray:
     """
     Calculate the Cowrin-Schultz spread.
 
-    See page 285 of Advances in Financial Machine Learning by Marcos Lopez de Prado.
+
 
     Parameters
     ----------
-    high : np.ndarray
-        The high prices.
-    low : np.ndarray
-        The low prices.
+    p_high : np.ndarray
+        The p_high prices.
+    p_low : np.ndarray
+        The p_low prices.
     window : int
         The window size.
 
@@ -51,30 +53,62 @@ def cowrin_schultz_spread(high: np.ndarray, low: np.ndarray, window: int) -> np.
     -------
     np.ndarray
         The Cowrin-Schultz spread.
+
+    References
+    ----------
+    See page 285 of Advances in Financial Machine Learning by Marcos Lopez de Prado.
     """
-    beta = _get_beta(high=high, low=low, window=window)
-    gamma = _get_gamma(high=high, low=low)
+    beta = _get_beta(p_high=p_high, p_low=p_low, window=window)
+    gamma = _get_gamma(p_high=p_high, p_low=p_low)
     alpha = _get_alpha(beta=beta, gamma=gamma)
     spread = 2 * (np.exp(alpha) - 1) / (np.exp(alpha) + 1)
     return spread
 
 
+def roll_spread(p_close: np.ndarray, window: int) -> np.ndarray:
+    """
+    Calculate the Roll spread.
+
+    Parameters
+    ----------
+    p_close : np.ndarray
+        The p_close prices.
+    window : int
+        The window size.
+
+    Returns
+    -------
+    np.ndarray
+        The Roll spread
+
+    References
+    ----------
+    https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3270269
+    """
+    logc = np.log(p_close)
+    delta_logc = logc.diff()
+    delta_logc_shift = np.roll(delta_logc, shift=1)
+    delta_logc_shift[0] = 0
+    roll = moving_cov(x=np.stack([delta_logc, delta_logc_shift], axis=1), window_size=window)
+    return roll
+
+
 def garman_klass_volatility(
-    high: np.ndarray, low: np.ndarray, open: np.ndarray, close: np.ndarray, window: int
+    p_high: np.ndarray, p_low: np.ndarray, p_open: np.ndarray, p_close: np.ndarray, window: int
 ) -> np.ndarray:
     """
     Calculate the Garman-Klass volatility.
 
     Parameters
     ----------
-    high : np.ndarray
-        The high prices.
-    low : np.ndarray
-        The low prices.
-    open : np.ndarray
-        The open prices.
-    close : np.ndarray
-        The close prices.
+    p_high : np.ndarray
+        The p_high prices.
+    p_low : np.ndarray
+        The p_low prices.
+    p_open : np.ndarray
+        The p_open prices.
+    p_close : np.ndarray
+        The p_close prices.
     window : int
         The window size.
 
@@ -88,8 +122,8 @@ def garman_klass_volatility(
     Volatility Modelling and Trading by Artur Sepp
     https://papers.ssrn.com/sol3/papers.cfm?abstract_id=2810768
     """
-    returns_sq = 0.5 * np.log(high / low) ** 2
-    returns_close_open = (2 * np.log(2) - 1) * (np.log(close / open)) ** 2
+    returns_sq = 0.5 * np.log(p_high / p_low) ** 2
+    returns_close_open = (2 * np.log(2) - 1) * (np.log(p_close / p_open)) ** 2
     sigma_sq = moving_average(x=returns_sq, window_size=window) - moving_average(
         x=returns_close_open, window_size=window
     )
@@ -97,22 +131,21 @@ def garman_klass_volatility(
 
 
 def rogers_satchell_volatility(
-    high: np.ndarray, low: np.ndarray, open: np.ndarray, close: np.ndarray, window: int
+    p_high: np.ndarray, p_low: np.ndarray, p_open: np.ndarray, p_close: np.ndarray, window: int
 ) -> np.ndarray:
     """
     Calculate the Rogers-Satchell volatility.
-    p 20, Volatility Modelling and Trading
 
     Parameters
     ----------
-    high : np.ndarray
-        The high prices.
-    low : np.ndarray
-        The low prices.
-    open : np.ndarray
-        The open prices.
-    close : np.ndarray
-        The close prices.
+    p_high : np.ndarray
+        The p_high prices.
+    p_low : np.ndarray
+        The p_low prices.
+    p_open : np.ndarray
+        The p_open prices.
+    p_close : np.ndarray
+        The p_close prices.
     window : int
         The window size.
 
@@ -127,27 +160,28 @@ def rogers_satchell_volatility(
     https://papers.ssrn.com/sol3/papers.cfm?abstract_id=2810768
     """
     sigma_sq = moving_average(
-        x=np.log(high / close) * np.log(high / open) + np.log(low / close) * np.log(low / open), window_size=window
+        x=np.log(p_high / p_close) * np.log(p_high / p_open) + np.log(p_low / p_close) * np.log(p_low / p_open),
+        window_size=window,
     )
     return sigma_sq
 
 
 def yang_zhang_volatility(
-    high: np.ndarray, low: np.ndarray, open: np.ndarray, close: np.ndarray, window: int
+    p_high: np.ndarray, p_low: np.ndarray, p_open: np.ndarray, p_close: np.ndarray, window: int
 ) -> np.ndarray:
     """
     Calculate the Yang-Zhang volatility.
 
     Parameters
     ----------
-    high : np.ndarray
-        The high prices.
-    low : np.ndarray
-        The low prices.
-    open : np.ndarray
-        The open prices.
-    close : np.ndarray
-        The close prices.
+    p_high : np.ndarray
+        The p_high prices.
+    p_low : np.ndarray
+        The p_low prices.
+    p_open : np.ndarray
+        The p_open prices.
+    p_close : np.ndarray
+        The p_close prices.
     window : int
         The window size.
 
@@ -163,33 +197,32 @@ def yang_zhang_volatility(
     """
     coeff = 0.34 / (1.34 + (window + 1) / (window - 1))
 
-    open_prev_close_ret = np.log(open / close.shift(1))
-    close_prev_open_ret = np.log(close / open.shift(1))
+    open_prev_close_ret = np.log(p_open / p_close.shift(1))
+    close_prev_open_ret = np.log(p_close / p_open.shift(1))
 
     sigma_overnight_sq = moving_average(x=open_prev_close_ret**2, window_size=window)
     sigma_open_to_close_sq = moving_average(x=close_prev_open_ret**2, window_size=window)
 
-    sigma_rs_sq = rogers_satchell_volatility(high=high, low=low, close=close, open=open, window=window)
+    sigma_rs_sq = rogers_satchell_volatility(p_high=p_high, p_low=p_low, p_close=p_close, p_open=p_open, window=window)
 
     sigma_sq = sigma_overnight_sq + coeff * sigma_open_to_close_sq + (1 - coeff) * sigma_rs_sq
     return sigma_sq
 
 
-def becker_parkinson_volatility(high: np.ndarray, low: np.ndarray, window: int) -> np.ndarray:
+def becker_parkinson_volatility(p_high: np.ndarray, p_low: np.ndarray, window: int) -> np.ndarray:
     """Calculate the Becker-Parkinson volatility.
 
-    See page 285 of Advances in Financial Machine Learning by Marcos Lopez de Prado.
     This volatility is useful in the corporate bond market, where there is no centralized order book, and
     trades occur through bids wanted in competition (BWIC).
 
     Parameters
     ----------
-    high : np.ndarray
-        The high prices.
-    low : np.ndarray
-        The low prices.
-    close : np.ndarray
-        The close prices.
+    p_high : np.ndarray
+        The p_high prices.
+    p_low : np.ndarray
+        The p_low prices.
+    p_close : np.ndarray
+        The p_close prices.
     window : int
         The window size.
 
@@ -197,22 +230,27 @@ def becker_parkinson_volatility(high: np.ndarray, low: np.ndarray, window: int) 
     -------
     np.ndarray
         The Becker-Parkinson volatility.
+
+    References
+    ----------
+    https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3270269
+    See also page 285 of Advances in Financial Machine Learning by Marcos Lopez de Prado.
     """
-    beta = _get_beta(high=high, low=low, window=window)
-    gamma = _get_gamma(high=high, low=low)
+    beta = _get_beta(p_high=p_high, p_low=p_low, window=window)
+    gamma = _get_gamma(p_high=p_high, p_low=p_low)
     sigma = _get_sigma(beta=beta, gamma=gamma)
     return sigma
 
 
-def _get_gamma(high: np.ndarray, low: np.ndarray) -> np.ndarray:
-    h2 = _moving_generic_1(x=high, window_func=partial(np.median, axis=1), window_size=2, post=f"_max{2}")
-    l2 = _moving_generic_1(x=low, window_func=partial(np.median, axis=1), window_size=2, post=f"_min{2}")
+def _get_gamma(p_high: np.ndarray, p_low: np.ndarray) -> np.ndarray:
+    h2 = _moving_generic_1(x=p_high, window_func=partial(np.median, axis=1), window_size=2, post=f"_max{2}")
+    l2 = _moving_generic_1(x=p_low, window_func=partial(np.median, axis=1), window_size=2, post=f"_min{2}")
     gamma = np.log(h2 / l2) ** 2.0
     return gamma
 
 
-def _get_beta(high: np.ndarray, low: np.ndarray, window: int) -> np.ndarray:
-    h1 = np.log(high / low) ** 2.0
+def _get_beta(p_high: np.ndarray, p_low: np.ndarray, window: int) -> np.ndarray:
+    h1 = np.log(p_high / p_low) ** 2.0
     beta = _moving_generic_1(x=h1, window_func=partial(np.sum, axis=1), window_size=2, post=f"_sum{2}")
     beta = moving_average(x=beta, window_size=window)
     return beta
--- quantfinlib/feature/transform/moving_window_func.py
+++ quantfinlib/feature/transform/moving_window_func.py
@@ -3,8 +3,10 @@
 from functools import partial
 from typing import Callable
 import numpy as np
+import pandas as pd
 
 from quantfinlib.util.convert import type_to_np, np_to_type
+from quantfinlib.util._inspect import _num_columns
 
 
 def _moving_window_func_c1(x: np.ndarray, window_func: Callable, window_size: int = 30) -> np.ndarray:
@@ -47,6 +49,36 @@ def _moving_window_func_c1(x: np.ndarray, window_func: Callable, window_size: in
     ans[: window_size - 1, :] = np.NaN
     ans[window_size - 1 :, :] = y
     return ans.reshape(x_shape)
+
+
+def _moving_window_func_cn(x: np.ndarray, window_func: Callable, window_size: int = 30, transpose: bool = False):
+    # trivial checks
+    assert window_size > 1
+    assert window_size <= len(x)
+    assert x.ndim in (1, 2)
+    # vectors are converted to a 1 col matirx. Everything below assumed that x is a matrix
+    if x.ndim == 1:
+        x = x.reshape(-1, 1)
+    num_rows = x.shape[0]
+    # create a 3D view: [num_rows - window_size + 1, num_cols, window_size]
+    y = np.lib.stride_tricks.sliding_window_view(x, window_shape=(window_size, 1)).squeeze(axis=3)
+    ans = None
+    # loop over all windows
+    for i in range(y.shape[0]):
+        # call the transformation function
+        if transpose:
+            z = window_func(y[i, ...].T)
+        else:
+            z = window_func(y[i, ...])
+        # flatten the transformation output to a vector
+        z = z.flatten()
+        # allocate return data if we had not already
+        if ans is None:
+            ans = np.empty((num_rows, len(z)), dtype=float)
+            ans[: window_size - 1, :] = np.NaN
+        # store the window result in the output matrix
+        ans[i + window_size - 1, :] = z
+    return ans
 
 
 def _moving_generic_1(x, window_func, window_size=30, post=""):
@@ -93,4 +125,37 @@ def moving_average(x, window_size=30):
     array-like
         Moving averages.
     """
-    return _moving_generic_1(x, partial(np.mean, axis=1), window_size, f'_average{window_size}')
+    return _moving_generic_1(x, partial(np.mean, axis=1), window_size, f"_average{window_size}")
+
+
+def moving_cov(x, window_size=30):
+    r"""Compute the windowed moving covariance.
+
+    Parameters
+    ----------
+    x : array-like
+        Input 2d array, with individual time series in the columns.
+    window_size : int, default=30
+        Window size.
+
+    Returns
+    -------
+    array-like
+        Sliding window covariance. Returns the elements of the upper
+        triangular part of the covariance matrix.
+    """
+    assert _num_columns(x) > 0
+    columns = None
+    if isinstance(x, pd.DataFrame):
+        ci, cj = np.triu_indices(x.shape[1])
+        columns = [f"{x.columns[i]}_{x.columns[j]}" for i, j in zip(ci, cj)]
+    return np_to_type(
+        _moving_window_func_cn(
+            type_to_np(x),
+            lambda a: np.atleast_2d(np.cov(a, rowvar=False))[np.triu_indices(a.shape[0])],
+            window_size=window_size,
+        ),
+        x,
+        columns=columns,
+        post=f"_cov{window_size}",
+    )
